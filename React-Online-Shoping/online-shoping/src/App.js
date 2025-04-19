0a924c9f0a23c09557716fa595c04a867d5f42e2
Sanamneelima123 <bkusuma19@gmail.com>
2025-04-19 12:12:41 +0530

    Remove secret from file

--- React-Online-Shoping/online-shoping/src/App.js
+++ React-Online-Shoping/online-shoping/src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
 import Home from './pages/Home';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Register from './register/Register';
+import Login from './login/Login';
+import AllProducts from './pages/all products/All Products';
+import SingleProduct from './single product/SingleProduct';
 
 function App() {
   return (
       <div>
-        <Home/>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path='/register' element={<Register/>} />
+            <Route path='/Login' element={<Login />} />
+            <Route path='/products' element={<AllProducts/>} />
+            <Route path='/products/:id' element={<SingleProduct/>} />
+          </Routes>
+        </Router>
       </div>
   );
 }
